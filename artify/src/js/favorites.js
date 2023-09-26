c906a43b2e8c33ea6572a7fa57b21cca1c340fe4
Vyshnav Nair <31135506+vysh1234vyshnav1223@users.noreply.github.com>
2023-09-27 00:59:26 +0530

    Update favorites.js

--- artify/src/js/favorites.js
+++ artify/src/js/favorites.js
@@ -1,11 +1,13 @@
-import { initializeLikedArtworks } from "./imageDisplayer";
-import { displayLikedArtworks, displayNoFavoritesPopup } from "./imageDisplayer";
+import { displayLikedArtworks, displayNoFavoritesPopup, initializeLikedArtworks } from "./imageDisplayer";
 
 //Displays the favorites tab when clicked on it unless there's no liked pictures. Also if no liked images exists, a popup appears with a message.
 const favoritesTab = document.getElementById('favourites');
+let likedArtworks =  initializeLikedArtworks();
 
 favoritesTab.addEventListener('click', () => {
-  if (initializeLikedArtworks().length === 0) {
+  likedArtworks = initializeLikedArtworks();
+  console.log(likedArtworks);
+  if (likedArtworks.length === 0) {
     displayNoFavoritesPopup(); 
   }else{
     displayLikedArtworks();
